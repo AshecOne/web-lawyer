53629bf594d40a8dbe1cc724805bf90771cc185f
AshecOne <haseyong123@gmail.com>
2024-03-26 09:07:31 +0700

    dikembalikan

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,23 @@
-import dynamic from "next/dynamic";
-import React, { Suspense } from "react";
-
-const Hero = dynamic(() => import("@/views/Hero"), { suspense: true });
-const Practice = dynamic(() => import("@/views/Practice"), { suspense: true });
-const Stats = dynamic(() => import("@/views/Stats"), { suspense: true });
-const Attorneys = dynamic(() => import("@/views/Attorneys"), {
-  suspense: true,
-});
-const Overview = dynamic(() => import("@/views/Overview"), { suspense: true });
-const Testimoni = dynamic(() => import("@/views/Testimoni"), {
-  suspense: true,
-});
-const Consultation = dynamic(() => import("@/views/Consultation"), {
-  suspense: true,
-});
-const Map = dynamic(() => import("@/views/Map"), { suspense: true });
+import Hero from "@/views/Hero";
+import Map from "@/views/Map";
+import Consultation from "@/views/Consultation";
+import Testimoni from "@/views/Testimoni";
+import Overview from "@/views/Overview";
+import Stats from "@/views/Stats";
+import Practice from "@/views/Practice";
+import Attorneys from "@/views/Attorneys";
 
 export default function Home() {
   return (
     <div>
-      <Suspense fallback={<div></div>}>
-        <Hero />
-      </Suspense>
-      <Suspense fallback={<div></div>}>
-        <Practice />
-      </Suspense>
-      <Suspense fallback={<div></div>}>
-        <Stats />
-      </Suspense>
-      <Suspense fallback={<div></div>}>
-        <Attorneys />
-      </Suspense>
-      <Suspense fallback={<div></div>}>
-        <Overview />
-      </Suspense>
-      <Suspense fallback={<div></div>}>
-        <Testimoni />
-      </Suspense>
-      <Suspense fallback={<div></div>}>
-        <Consultation />
-      </Suspense>
-      <Suspense fallback={<div></div>}>
-        <Map />
-      </Suspense>
+      <Hero />
+      <Practice />
+      <Stats />
+      <Attorneys />
+      <Overview />
+      <Testimoni />
+      <Consultation />
+      <Map />
     </div>
   );
 }
